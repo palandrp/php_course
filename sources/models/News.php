ac433c8673c4e6ece8eda2e228e4d1b1f1705544
Pavel Petrikovskiy <petrikovskiy@lanit.ru>
2019-06-21 19:26:36 +0500

    Adds getNewsList to model News

--- sources/models/News.php
+++ sources/models/News.php
@@ -17,7 +17,29 @@ class News
      */
     public static function getNewsList()
     {
-        // Запрос к БД
+        $host = 'localhost';
+        $dbname = 'mvc_site';
+        $user = 'root';
+        $password = '';
+        $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+
+        $newsList = array();
+
+        $result = $db->query('SELECT id, title, date, short_content '
+            . 'FROM news '
+            . 'ORDER BY date DESC '
+            . 'LIMIT 10');
+
+        $i = 0;
+        while($row = $result->fetch()) {
+            $newsList[$i]['id'] = $row['id'];
+            $newsList[$i]['title'] = $row['title'];
+            $newsList[$i]['date'] = $row['date'];
+            $newsList[$i]['short_content'] = $row['short_content'];
+            $i++;
+        }
+
+        return $newsList;
     }
 
 }
